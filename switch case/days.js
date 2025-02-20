969e07c8d8f12118cdcb7afa44703081282579f1
Romesh Jain <romeshjain73@gmail.com>
2025-02-20 12:25:48 +0530

    add switch case

---
+++ switch case/days.js
@@ -0,0 +1,54 @@
+
+//? SWITCH CASE  
+     //!FALL THROUGH - IF YOU DONT PROVIDE ANYH BREAK CONDITION 
+
+     let day = prompt('Enter a day')
+
+     switch (day){
+         case "monday":{
+             console.log("its a moday")
+             break;
+     
+         }
+         case "tuesday":{
+             console.log("its tuew")
+             break;
+      
+     
+         }
+         case "wednesday":{
+             console.log("its a wed")
+             break;
+     
+     
+         }
+         case "thurday":{
+             console.log("its a thuer")
+             break;
+     
+     
+         }
+         case "friday":{
+             console.log("its a fr")
+             break;
+     
+     
+         }
+         case "saturday":{
+             console.log("its a satur")
+             break;
+     
+     
+         }
+         case "sunday":{
+             console.log("its a sun")
+             break;
+     
+     
+         }
+         default:{
+             console.log("invalid")
+             break;
+     
+         }
+     } 
